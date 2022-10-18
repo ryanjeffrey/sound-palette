fdeba5b0d975ee3b3308c7cea8ee9109878e8610
Nathan Burgess <nathans.email.address@gmail.com>
2022-10-18 16:13:25 -0700

    added color pickers above keyboard

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ import { PaletteKeyboard } from './components/piano/PaletteKeyboard';
 import About from './components/about/About';
 
 function App() {
-  const { currentBackground } = useContext(ColorContext);
+  const { currentBackground, keyboardPalette, setKeyboardPalette } = useContext(ColorContext);
 
   // if (currentBackground.length === 1) {
   //   const single = currentBackground;
   // }
+  const colorKeyArray = Object.keys(keyboardPalette);
+
 
   return (
     <div className="App" style={currentBackground.length < 2 ? { background: currentBackground[0] } : { background: `linear-gradient(90deg, ${currentBackground.join(',')})` }}>
       <Header />
-      <input type = 'color' />
+      {colorKeyArray.map((midiNumber, index) => {
+        return <input key = {index} type = 'color' value={keyboardPalette[midiNumber]} />;
+      })}
       <Switch>
         <Route path="/about" component={About} />
         <Route path="/" component={PaletteKeyboard} />
