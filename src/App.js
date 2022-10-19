bc9d1c96a7f9dba4a8e537987b1e521520df8d34
Nathan Burgess <nathans.email.address@gmail.com>
2022-10-18 17:14:48 -0700

    color picker works

--- src/App.js
+++ src/App.js
@@ -14,12 +14,28 @@ function App() {
   // }
   const colorKeyArray = Object.keys(keyboardPalette);
 
-
   return (
     <div className="App" style={currentBackground.length < 2 ? { background: currentBackground[0] } : { background: `linear-gradient(90deg, ${currentBackground.join(',')})` }}>
       <Header />
       {colorKeyArray.map((midiNumber, index) => {
-        return <input key = {index} type = 'color' value={keyboardPalette[midiNumber]} />;
+        return <input key = {index} type = 'color' value={keyboardPalette[midiNumber]} onChange={(e) => {
+          function makeNewKeyboardPalette() {
+            let newKeyboardPalette = {};
+            for (let key in keyboardPalette) {
+              key === midiNumber ? newKeyboardPalette[key] = e.target.value : newKeyboardPalette[key] = keyboardPalette[key];
+            }
+            return newKeyboardPalette;
+          }
+          setKeyboardPalette(makeNewKeyboardPalette());
+
+        }
+        }    
+        // this color picker has a specific midiNumber
+        // the user has picked a new color e.target.value
+        // we want to build a new object to set as keyboardPalette
+        // it's the same as the old one, with current midiNumber set to color user picked
+
+        />;
       })}
       <Switch>
         <Route path="/about" component={About} />
