7353eaf4a4e053355ac1f32da32be0765cf62450
Jmart5564 <Jmart5564@gmail.com>
2022-10-18 17:29:44 -0700

    CSS on color inputs

--- src/App.js
+++ src/App.js
@@ -17,26 +17,29 @@ function App() {
   return (
     <div className="App" style={currentBackground.length < 2 ? { background: currentBackground[0] } : { background: `linear-gradient(90deg, ${currentBackground.join(',')})` }}>
       <Header />
-      {colorKeyArray.map((midiNumber, index) => {
-        return <input key = {index} type = 'color' value={keyboardPalette[midiNumber]} onChange={(e) => {
-          function makeNewKeyboardPalette() {
-            let newKeyboardPalette = {};
-            for (let key in keyboardPalette) {
-              key === midiNumber ? newKeyboardPalette[key] = e.target.value : newKeyboardPalette[key] = keyboardPalette[key];
+      <div className="color-div">
+        {colorKeyArray.map((midiNumber, index) => {
+          return <input key = {index} type = 'color' value={keyboardPalette[midiNumber]} onChange={(e) => {
+            function makeNewKeyboardPalette() {
+              let newKeyboardPalette = {};
+              for (let key in keyboardPalette) {
+                key === midiNumber ? newKeyboardPalette[key] = e.target.value : newKeyboardPalette[key] = keyboardPalette[key];
+              }
+              return newKeyboardPalette;
             }
-            return newKeyboardPalette;
-          }
-          setKeyboardPalette(makeNewKeyboardPalette());
+            setKeyboardPalette(makeNewKeyboardPalette());
 
-        }
-        }    
+          }
+          }    
         // this color picker has a specific midiNumber
         // the user has picked a new color e.target.value
         // we want to build a new object to set as keyboardPalette
         // it's the same as the old one, with current midiNumber set to color user picked
 
-        />;
-      })}
+          />;
+        
+        })}
+      </div>
       <Switch>
         <Route path="/about" component={About} />
         <Route path="/" component={PaletteKeyboard} />
