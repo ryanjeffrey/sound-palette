abb84ee98455f76a8b2a6fadc6f389102e9b0b6d
Nathan Burgess <nathans.email.address@gmail.com>
2022-10-19 19:13:41 -0700

    refactored octave buttons into Octaves component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import { ColorContext } from './ColorContext';
@@ -8,45 +8,10 @@ import About from './components/about/About';
 import Select from './components/Select';
 import Visualizer from './components/Visualizer';
 import ColorPicker from './components/ColorPicker';
-import { InstrumentContext } from './InstrumentContext';
+import Octaves from './components/octaves/Octaves';
 
 function App() {
-  const { currentBackground, keyboardPalette, setKeyboardPalette } = useContext(ColorContext);
-  const { setNote, note } = useContext(InstrumentContext);
-  
-
-  const hotkey = useCallback(
-    (e) => {
-      if (e.key === 'z') {
-        if (note > 1) {
-          setNote((prevNote) => prevNote - 1);
-          let newKeyboardPalette = {};
-          for (let number in keyboardPalette) {
-            newKeyboardPalette[Number(number) - 12] = keyboardPalette[number];
-          }
-          setKeyboardPalette(newKeyboardPalette);
-        }
-      }
-      if (e.key === 'x') {
-        if (note < 6) {
-          setNote((prevNote) => prevNote + 1);
-          let newKeyboardPalette = {};
-          for (let number in keyboardPalette) {
-            newKeyboardPalette[Number(number) + 12] = keyboardPalette[number];
-          }
-          setKeyboardPalette(newKeyboardPalette);
-        }
-      }
-    },
-    [note, setNote, keyboardPalette, setKeyboardPalette]
-  );
-
-  useEffect(() => {
-    window.addEventListener('keydown', hotkey);
-    return () => {
-      window.removeEventListener('keydown', hotkey);
-    };
-  }, [hotkey]);
+  const { currentBackground } = useContext(ColorContext);
 
   return (
     <div
@@ -64,34 +29,7 @@ function App() {
           <Visualizer />
           <ColorPicker />
           <PaletteKeyboard />
-          <button
-            onClick={() => {
-              if (note < 6) {
-                setNote(note + 1);
-                let newKeyboardPalette = {};
-                for (let number in keyboardPalette) {
-                  newKeyboardPalette[Number(number) + 12] = keyboardPalette[number];
-                }
-                setKeyboardPalette(newKeyboardPalette);
-              }
-            }}
-          >
-            Octave Up (Press x)
-          </button>
-          <button
-            onClick={() => {
-              if (note > 1) {
-                setNote(note - 1);
-                let newKeyboardPalette = {};
-                for (let number in keyboardPalette) {
-                  newKeyboardPalette[Number(number) - 12] = keyboardPalette[number];
-                }
-                setKeyboardPalette(newKeyboardPalette);
-              }
-            }}
-          >
-            Octave Down (Press z)
-          </button>
+          <Octaves />
           <Select />
         </Route>
       </Switch>
---
+++ src/components/octaves/Octaves.js
@@ -0,0 +1,76 @@
+import { useCallback, useContext, useEffect } from 'react';
+import { ColorContext } from '../../ColorContext';
+import { InstrumentContext } from '../../InstrumentContext';
+import './Octaves.css';
+
+export default function Octaves() {
+  const { keyboardPalette, setKeyboardPalette } = useContext(ColorContext);
+  const { setNote, note } = useContext(InstrumentContext);
+
+  const hotkey = useCallback(
+    (e) => {
+      if (e.key === 'z') {
+        if (note > 1) {
+          setNote((prevNote) => prevNote - 1);
+          let newKeyboardPalette = {};
+          for (let number in keyboardPalette) {
+            newKeyboardPalette[Number(number) - 12] = keyboardPalette[number];
+          }
+          setKeyboardPalette(newKeyboardPalette);
+        }
+      }
+      if (e.key === 'x') {
+        if (note < 6) {
+          setNote((prevNote) => prevNote + 1);
+          let newKeyboardPalette = {};
+          for (let number in keyboardPalette) {
+            newKeyboardPalette[Number(number) + 12] = keyboardPalette[number];
+          }
+          setKeyboardPalette(newKeyboardPalette);
+        }
+      }
+    },
+    [note, setNote, keyboardPalette, setKeyboardPalette]
+  );
+
+  useEffect(() => {
+    window.addEventListener('keydown', hotkey);
+    return () => {
+      window.removeEventListener('keydown', hotkey);
+    };
+  }, [hotkey]);
+  return (
+    <div className='octave-button-container'>
+
+      <button className='octave-button'
+        onClick={() => {
+          if (note > 1) {
+            setNote(note - 1);
+            let newKeyboardPalette = {};
+            for (let number in keyboardPalette) {
+              newKeyboardPalette[Number(number) - 12] = keyboardPalette[number];
+            }
+            setKeyboardPalette(newKeyboardPalette);
+          }
+        }}
+      >
+            Octave Down (Press z)
+      </button>
+
+      <button className='octave-button'
+        onClick={() => {
+          if (note < 6) {
+            setNote(note + 1);
+            let newKeyboardPalette = {};
+            for (let number in keyboardPalette) {
+              newKeyboardPalette[Number(number) + 12] = keyboardPalette[number];
+            }
+            setKeyboardPalette(newKeyboardPalette);
+          }
+        }}
+      >
+            Octave Up (Press x)
+      </button>
+    </div>
+  );
+}
